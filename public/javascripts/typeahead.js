e7a98cdf2b76fd562de56ebf16480eb5970f9790
Chris Duxbury <chris_duxbury@outlook.com>
2024-01-09 16:38:18 +0800

    Bind API data with index page

--- public/javascripts/typeahead.js
+++ public/javascripts/typeahead.js
@@ -3,8 +3,20 @@ const typeahead = {
         this.input = document.getElementById("statesbar");
         if (!this.input) return;
         this.input.addEventListener("input", this.handleInput.bind(this));
+        this.matches = document.getElementById("matches");
+        fetch("/states")
+            .then(response => response.json())
+            .then(data => {
+                this.states = data.states;
+                for (const state of this.states) {
+                    const item = document.createElement("li");
+                    item.innerHTML = state;
+                    this.matches.appendChild(item);
+                }
+            });
     },
     handleInput: function() {
+
         return;
     }
 };
