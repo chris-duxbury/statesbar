4bdb8647a547364689f7cae7a44618feb917d30b
Chris Duxbury <chris_duxbury@outlook.com>
2024-01-09 18:56:55 +0800

    Updated list functionality

--- public/javascripts/typeahead.js
+++ public/javascripts/typeahead.js
@@ -1,8 +1,10 @@
 const typeahead = {
+    selectedIndex: -1,
     init: function() {
         this.input = document.getElementById("statesbar");
         if (!this.input) return;
         this.input.addEventListener("input", this.handleInput.bind(this));
+        this.input.addEventListener("keydown", this.handleKeyDown.bind(this));
         this.matches = document.getElementById("matches");
         fetch("/states")
             .then(response => response.json())
@@ -12,6 +14,10 @@ const typeahead = {
                     const item = document.createElement("li");
                     item.innerHTML = state;
                     this.matches.appendChild(item);
+                    item.addEventListener("click", () => {
+                        this.input.value = state;
+                        this.handleInput();
+                    });
                 }
             });
     },
@@ -28,7 +34,6 @@ const typeahead = {
                 if (!starts.test(a.name) && starts.test(b.name)) return 1;
                 return a.name < b.name ? -1 : 1;
             });
-        console.log(results);
         for (const state of results) {
             const item = document.createElement("li");
             const matchedText = match.exec(state)[0];
@@ -38,9 +43,37 @@ const typeahead = {
                     "<strong>" + matchedText + "</strong>"
                 );
             this.matches.appendChild(item);
+            item.addEventListener("click", () => {
+                this.input.value = state;
+                this.handleInput();
+            });
+        }
+    },
+    handleKeyDown: function(event) {
+        const { keyCode } = event;
+        const results = this.matches.children;
+        if (keyCode === 40 && this.selectedIndex < results.length - 1) {
+            this.selectedIndex++;
+        } else if (keyCode === 38 && this.selectedIndex >= 0) {
+            this.selectedIndex--;
+        } else if (keyCode === 13 && results[this.selectedIndex]) {
+            event.preventDefault();
+            const strong = new RegExp("<\/?strong>", "g");
+            this.input.value = results[this.selectedIndex].innerHTML.replace(strong, '');
+            this.handleInput();
+        }
+        for (let i = 0; i < results.length; i++) {
+            const result = results[i];
+            const selectedClass = "selected";
+            if (i === this.selectedIndex) {
+                result.classList.add(selectedClass);
+            } else if (result.classList.contains(selectedClass)) {
+                result.classList.remove(selectedClass);
+            }
         }
     },
     clearResults: function() {
+        this.selectedIndex = -1;
         while (this.matches.firstChild) {
             this.matches.removeChild(this.matches.firstChild);
         }
