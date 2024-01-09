8870233b3ad953cd290aa22a85d127f67ab83703
Chris Duxbury <chris_duxbury@outlook.com>
2024-01-09 15:18:03 +0800

    Remove view engine

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ var statesRouter = require('./routes/states');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -33,9 +29,10 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(500).json({
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
