510cdaa76e0add5bd1877d797baace946c8848df
Chris Duxbury <chris_duxbury@outlook.com>
2024-01-10 10:34:42 +0800

    Add comments

--- public/javascripts/typeahead.js
+++ public/javascripts/typeahead.js
@@ -15,14 +15,18 @@ const typeahead = {
     handleInput: function() {
         this.clearResults();
         let { value } = this.input;
+        // if no text or a state has been exactly entered
         if (value.length < 1 || this.states.includes(value)) {
             value = ".*";
             this.matches.style.display = "none";
         } else {
             this.matches.style.display = "block";
         };
+        // states that begins the same as text
         const starts = new RegExp("^" + value, "i");
+        // states that includes text
         const match = new RegExp(value, "i");
+        // filter states by above match
         const results = this.states
             .filter(state => match.test(state))
             .sort((a, b) => {
@@ -30,6 +34,7 @@ const typeahead = {
                 if (!starts.test(a.name) && starts.test(b.name)) return 1;
                 return a.name < b.name ? -1 : 1;
             });
+        // no matches
         if (results.length === 0) {
             this.matches.style.display = "none";
         }
@@ -37,6 +42,7 @@ const typeahead = {
             const item = document.createElement("li");
             const matchedText = match.exec(state)[0];
             item.innerHTML = value == ".*" ? state :
+                // bold matched part of state
                 state.replace(
                     matchedText,
                     "<strong>" + matchedText + "</strong>"
@@ -51,16 +57,22 @@ const typeahead = {
     handleKeyDown: function(event) {
         const { keyCode } = event;
         const results = this.matches.children;
+        // down key
         if (keyCode === 40 && this.selectedIndex < results.length - 1) {
             this.selectedIndex++;
+        // up key
         } else if (keyCode === 38 && this.selectedIndex >= 0) {
             this.selectedIndex--;
+        // enter key
         } else if (keyCode === 13 && results[this.selectedIndex]) {
             event.preventDefault();
             const strong = new RegExp("<\/?strong>", "g");
+
+            // change search text to entered state
             this.input.value = results[this.selectedIndex].innerHTML.replace(strong, '');
             this.handleInput();
         }
+        // find selected state from drop down
         for (let i = 0; i < results.length; i++) {
             const result = results[i];
             const selectedClass = "selected";
